0d8b9c67ce51dfaaa8c0a4151d3ce4b96493b4cf
Kaarththigan <48351119+EaaswaralingamKaarththigan@users.noreply.github.com>
2022-06-10 14:47:10 +0200

    Commit amélioration 10.06.2022

--- Application/TPI-Chat-EaaswaralingamKaarththigan/TPI-Chat-EaaswaralingamKaarththigan/creationcanalprive.Designer.cs
+++ Application/TPI-Chat-EaaswaralingamKaarththigan/TPI-Chat-EaaswaralingamKaarththigan/creationcanalprive.Designer.cs
@@ -34,31 +34,35 @@ namespace TPI_Chat_EaaswaralingamKaarththigan
             this.btnCreerNvCanal = new System.Windows.Forms.Button();
             this.Perso1Txt = new System.Windows.Forms.TextBox();
             this.Perso2Txt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 70);
+            this.label1.Location = new System.Drawing.Point(94, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Personne 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(125, 123);
+            this.label2.Location = new System.Drawing.Point(94, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Personne 2";
             // 
             // btnCreerNvCanal
             // 
-            this.btnCreerNvCanal.Location = new System.Drawing.Point(166, 179);
+            this.btnCreerNvCanal.Location = new System.Drawing.Point(124, 145);
+            this.btnCreerNvCanal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCreerNvCanal.Name = "btnCreerNvCanal";
-            this.btnCreerNvCanal.Size = new System.Drawing.Size(108, 54);
+            this.btnCreerNvCanal.Size = new System.Drawing.Size(81, 44);
             this.btnCreerNvCanal.TabIndex = 12;
             this.btnCreerNvCanal.Text = "Créer un nouveau canal";
             this.btnCreerNvCanal.UseVisualStyleBackColor = true;
@@ -66,28 +70,40 @@ namespace TPI_Chat_EaaswaralingamKaarththigan
             // 
             // Perso1Txt
             // 
-            this.Perso1Txt.Location = new System.Drawing.Point(212, 67);
+            this.Perso1Txt.Location = new System.Drawing.Point(159, 54);
+            this.Perso1Txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Perso1Txt.Name = "Perso1Txt";
-            this.Perso1Txt.Size = new System.Drawing.Size(119, 22);
+            this.Perso1Txt.Size = new System.Drawing.Size(90, 20);
             this.Perso1Txt.TabIndex = 13;
             // 
             // Perso2Txt
             // 
-            this.Perso2Txt.Location = new System.Drawing.Point(212, 120);
+            this.Perso2Txt.Location = new System.Drawing.Point(159, 98);
+            this.Perso2Txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Perso2Txt.Name = "Perso2Txt";
-            this.Perso2Txt.Size = new System.Drawing.Size(119, 22);
+            this.Perso2Txt.Size = new System.Drawing.Size(90, 20);
             this.Perso2Txt.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 207);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
             // 
             // creationcanalprive
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 296);
+            this.ClientSize = new System.Drawing.Size(348, 240);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.Perso2Txt);
             this.Controls.Add(this.Perso1Txt);
             this.Controls.Add(this.btnCreerNvCanal);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "creationcanalprive";
             this.Text = "creationcanalprive";
             this.ResumeLayout(false);
@@ -102,5 +118,6 @@ namespace TPI_Chat_EaaswaralingamKaarththigan
         private System.Windows.Forms.Button btnCreerNvCanal;
         private System.Windows.Forms.TextBox Perso1Txt;
         private System.Windows.Forms.TextBox Perso2Txt;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
